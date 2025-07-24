96f87d725dd287d809a916f3a418b13facedb42e
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-24 17:40:00 -0300

    Estruturas Condicionais 03

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -5,9 +5,9 @@ public class Aula05EstruturasCondicionais02 {
         // idade < 15 categoria infantil
         // idade >= 15 && idade < 18 categoria juvenil
         // idade >= 18 categoria adulto
-
         int idade = 12;
         String categoria;
+
         if (idade < 15) {
             categoria = "Categoria Infantil";
         } else if (idade >= 15 && idade < 18) {
@@ -15,6 +15,7 @@ public class Aula05EstruturasCondicionais02 {
         } else {
             categoria = "Categoria Adulto";
         }
+        categoria = idade < 15 ? "Categoria Infantil" : idade >= 15 && idade < 18 ? "Categoria Juvenil" : "Categoria Adulto";
         System.out.println(categoria);
     }
 }
