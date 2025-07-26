b6ce52f5e1e2adff2525ee53233a4f901526b01d
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-26 09:36:11 -0300

    Exercício

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Corsa";
+        carro1.modelo = "Classic";
+        carro1.ano = 2003;
+
+        carro2.nome = "Corolla";
+        carro2.modelo = "Confort";
+        carro2.ano = 2014;
+
+        System.out.println("Carro 1");
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("\nCarro 2");
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+    }
+}
