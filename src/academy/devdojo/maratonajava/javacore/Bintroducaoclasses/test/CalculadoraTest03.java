6240e25622e223d3c71d7d1cc606b790748ed0e7
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-07-27 00:14:13 -0300

    Métodos 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaoclasses/test/CalculadoraTest03.java
@@ -2,10 +2,13 @@ package academy.devdojo.maratonajava.javacore.Bintroducaoclasses.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaoclasses.dominio.Calculadora;
 
-public class CalculadorTest03 {
+public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.divideDoisNumeros(20, 0);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros02(20, 0));
+        System.out.println("------------------------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 0);
     }
 }
