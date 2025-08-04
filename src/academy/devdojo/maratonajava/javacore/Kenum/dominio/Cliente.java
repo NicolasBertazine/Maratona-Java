82b0263744da5e1d6ddea710abae2f0e4e845f66
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-04 19:56:14 -0300

    Enumeração pt 01 - Introdução

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Kenum.dominio;
+
+public class Cliente {
+    private String nome;
+    private String tipo;
+
+    public Cliente(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Kenum.test;
+
+
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        
+    }
+}
