eb6c04a3bf828063123a0075f4a612e29f639b83
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-09 16:33:29 -0300

    Classes abstratas pt 02 - Métodos abstratos

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
@@ -1,8 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
 
-public class Desenvolvedor extends Funcionario{
+public class Desenvolvedor extends Funcionario {
     public Desenvolvedor(String nome, double salario) {
         super(nome, salario);
+    }
+
+    @Override
+    public void calcularBonus() {
+        this.salario = this.salario + this.salario * 0.05;
     }
 
     @Override
