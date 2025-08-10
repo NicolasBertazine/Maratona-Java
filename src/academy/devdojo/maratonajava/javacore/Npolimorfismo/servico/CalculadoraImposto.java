1c5e902565fcc7ef243e1a3f31d7078393073fab
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-10 19:35:19 -0300

    Polimorfismo pt 04 - Cast e instanceof

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -8,8 +8,13 @@ public class CalculadoraImposto {
     public static void calcularImposto(Produto produto) {
         System.out.println("Relatório de imposto");
         double imposto = produto.calcularImposto();
-        System.out.println("Produto: "+produto.getNome());
-        System.out.println("Preço: "+produto.getValor());
-        System.out.println("Imposto a ser pago: "+imposto);
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getValor());
+        System.out.println("Imposto a ser pago: " + imposto);
+        if (produto instanceof Tomate) {
+            String dataValidade = ((Tomate) produto).getDataValidade();
+
+            System.out.println(dataValidade);
+        }
     }
 }
