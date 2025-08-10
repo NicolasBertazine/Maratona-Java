80c2511b019198adef2e877a8063d338f5d6b96c
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-10 20:24:04 -0300

    Exceções pt 02 - RuntimeException

--- src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowTest01.java
@@ -1,8 +1,8 @@
-package academy.devdojo.maratonajava.javacore.Oexception.test;
+package academy.devdojo.maratonajava.javacore.Oexception.error.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
-        
+
     }
 
     public static void recursividade() {
