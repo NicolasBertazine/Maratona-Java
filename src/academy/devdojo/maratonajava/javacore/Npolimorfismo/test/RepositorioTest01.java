c82ad0007fafbc7895c2d3612c5bcfd29edb9709
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-10 19:47:23 -0300

    Polimorfismo pt 05 - Programação orientada a interface

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioArquivo;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioMemorio;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RepositorioTest01 {
+    public static void main(String[] args) {
+        Repositorio repositorio = new RepositorioMemorio();
+        repositorio.salvar();
+        List<String> list = new LinkedList<>();
+        list.add("Goku");
+        list.add("Vegeta");
+        list.add("Kuririn");
+
+        System.out.println(list);
+    }
+}
