fa3d5b20b142e44064c0cb24fc26792e1d9d939b
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-14 17:11:20 -0300

    Strings pt 02 parte01

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -5,12 +5,13 @@ public class StringTest01 {
         String nome = "Nícolas"; //String constant pool
         String nome2 = "Nícolas";
 
-        nome.concat("Suane");
+        nome.concat(" Suane"); // nome += " Suane"
 
         System.out.println(nome);
         System.out.println(nome == nome2);
 
         String nome3 = new String("Nícolas"); // 1 variavel de referência, 2 objetos do tipo String, 3 uma string no pool de String
+
         System.out.println(nome2 == nome3);
         System.out.println(nome2 == nome3.intern());
     }
---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f", "l"));
+    }
+}
