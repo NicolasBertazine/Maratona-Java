ef12135b83e831eb07d3dd7676e1eac47e37092a
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-14 21:09:51 -0300

    Strings pt 02 parte02

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -2,9 +2,13 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 
 public class StringTest02 {
     public static void main(String[] args) {
-        String nome = "Luffy";
+        String nome = "   Luffy   ";
+        String numeros = "012345";
         System.out.println(nome.charAt(0));
         System.out.println(nome.length());
         System.out.println(nome.replace("f", "l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(numeros.substring(3, numeros.length()));
+        System.out.println(nome.trim());
     }
 }
