b2c5dacd1012eb322803a3c4695e69242949b620
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-14 21:26:19 -0300

    Strings pt 03 - Desempenho

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringPerformanceTest01.java
@@ -0,0 +1,46 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringPerformanceTest01 {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concaString(30_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String "+ (fim - inicio) + "ms");
+
+        System.out.println("_____________________");
+
+        inicio = System.currentTimeMillis();
+        concaStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder "+ (fim - inicio) + "ms");
+
+        System.out.println("_____________________");
+
+        inicio = System.currentTimeMillis();
+        concaStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer "+ (fim - inicio) + "ms");
+    }
+
+    private static void concaString(int tamanho) {
+        String texto = "Olá, meu nome é Nícolas";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    private static void concaStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    private static void concaStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+}
+
