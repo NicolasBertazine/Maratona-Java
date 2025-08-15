d47103eb1eb0088ffb51f002aafdabefd69f4a6d
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-14 21:38:06 -0300

    Strings pt 04 - StringBuilder

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "Nícolas Bertazine";
+        nome.concat(" Bertazine");
+        nome.substring(0,3);
+        System.out.println(nome);
+
+        StringBuilder sb = new StringBuilder("Nícolas Bertazine");
+        sb.append(" Bertazine").append(" Academy");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0,3);
+        System.out.println(sb);
+    }
+}
