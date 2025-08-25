d0a6d019e3d5a6a149165f0895aa43f6eec316cf
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-25 12:48:07 -0300

    Update PatterMatcherTest04.java

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatterMatcherTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatterMatcherTest04.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Uregex.test;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,8 +22,13 @@ public class PatterMatcherTest04 {
         // $
         // . 1.3 = 123, 133, 1@3, 1A3
 
-        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+\\.";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+\\.([a-zA-Z]+)+";
         String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com, sakura@mail";
+
+        System.out.println("Email valido");
+        System.out.println("zoro@mail.br".matches(regex));
+        System.out.println(texto.split(",")[1].trim());
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
