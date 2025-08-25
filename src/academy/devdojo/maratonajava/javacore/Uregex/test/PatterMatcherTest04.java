653ac613a87599f7e1d92cc4c710fe588e41ce89
Nícolas Bertazine <153151231+NicolasBertazine@users.noreply.github.com>
2025-08-25 12:40:19 -0300

    Create PatterMatcherTest04.java

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatterMatcherTest04.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatterMatcherTest04 {
+    public static void main(String[] args) {
+        // \d = Todos os dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todo caracter excluindo os brancos
+        // \w = a-z A-Z, todos os dígitos, _
+        // \W = Tudo que não for incluso no \w
+        // [] =
+        // ? Zero ou uma
+        // * Zero ou mais
+        // + uma ou mais
+        // {n, m} de n até m
+        // ()
+        // |
+        // $
+        // . 1.3 = 123, 133, 1@3, 1A3
+
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+\\.";
+        String texto = "luffy@hotmail.com, 123jotaro@gmail.com, #@!zoro@mail.br, teste@gmail.com, sakura@mail";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+
+        System.out.println("Texto: " + texto);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+
+        int numeroHex = 0xf;
+
+        System.out.println(numeroHex);
+
+    }
+}
